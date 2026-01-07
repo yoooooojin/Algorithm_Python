a84870a96e7320d64ee5d0c930d868ec3f2dd975
Yoojin Song <167386690+yoooooojin@users.noreply.github.com>
2026-01-07 16:17:00 +0900

    [level 2] Title: 올바른 괄호, Time: 8.78 ms, Memory: 9.55 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = True
+    stack = []
+    
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if len(stack) > 0:
+                stack.pop()
+            else:
+                answer = False
+                break
+    if len(stack) > 0:
+        answer = False
+    
+    return answer
