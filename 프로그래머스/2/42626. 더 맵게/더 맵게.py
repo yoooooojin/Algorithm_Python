54e284dfa39112afc60def6610e4c563b4c33121
Yoojin Song <167386690+yoooooojin@users.noreply.github.com>
2026-01-12 15:14:19 +0900

    [level 2] Title: 더 맵게, Time: 1701.04 ms, Memory: 48.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,17 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    
+    heapq.heapify(scoville)
+    
+    while scoville[0] < K:
+        new = heapq.heappop(scoville) + heapq.heappop(scoville) * 2
+        heapq.heappush(scoville, new)
+
+        answer += 1
+        
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
+    
+    return answer
